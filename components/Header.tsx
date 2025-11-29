9444d28b11d783cc2dc2215cc98c627f16c8b6dc
FrankBonanno <fb0023@mix.wvu.edu>
2025-11-28 22:38:13 -0500

    implement header

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,5 +1,7 @@
 import Image from 'next/image';
 import Link from 'next/link';
+import NavItems from '@/components/NavItems';
+import UserDropdown from '@/components/UserDropdown';
 
 const Header = () => {
   return (
@@ -15,9 +17,11 @@ const Header = () => {
           />
         </Link>
 
-        <nav className="hidden sm:block">{/* NavItems */}</nav>
+        <nav className="hidden sm:block">
+          <NavItems />
+        </nav>
 
-        {/* User Dropdown */}
+        <UserDropdown />
       </div>
     </header>
   );
