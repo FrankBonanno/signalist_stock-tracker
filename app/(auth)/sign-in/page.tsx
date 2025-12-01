f2508e6326ce20f54de2ae85a888d57be790723e
FrankBonanno <fb0023@mix.wvu.edu>
2025-12-01 17:10:37 -0500

    removed unused import

--- app/(auth)/sign-in/page.tsx
+++ app/(auth)/sign-in/page.tsx
@@ -4,7 +4,6 @@ import { useForm } from 'react-hook-form';
 import { Button } from '@/components/ui/button';
 import InputField from '@/components/forms/InputField';
 import FooterLink from '@/components/forms/FooterLink';
-import { useRouter } from 'next/navigation';
 
 const SignIn = () => {
   const {
